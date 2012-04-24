8d843190f3a15c55da0515eccd5d06f810329f1b
Antony Denyer <antonydenyer@hotmail.com>
2012-04-24 21:48:33 +0100

    implement first step

--- features/step_definitions/homepage.js
+++ features/step_definitions/homepage.js
@@ -1,18 +1,22 @@
+var Browser = require("zombie");
+
 var hompageSteps = function() {
-    this.Given(/^I am on the lnug homepage$/, function(callback) {
-        // express the regexp above with the code you wish you had
-        callback();
-    });
+	browser = new Browser();
 
-    this.Given(/^I register for the next lnug event$/, function(callback) {
-        // express the regexp above with the code you wish you had
-        callback();
-    });
+	this.Given(/^I am on the lnug homepage$/, function(callback) {
+		browser.visit("http://lnug.org", callback);
+	});
 
-    this.Then(/^I should be taken to eventbrite$/, function(callback) {
-        // express the regexp above with the code you wish you had
-        callback();
-    });
+	this.Given(/^I register for the next lnug event$/, function(callback) {
+		// express the regexp above with the code you wish you had
+		callback();
+	});
+
+	this.Then(/^I should be taken to eventbrite$/, function(callback) {
+		// express the regexp above with the code you wish you had
+		callback();
+	});
 
 }
 module.exports = hompageSteps;
+
