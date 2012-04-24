b9fbfa51cff73d84676a4f2aaf1025b9102d7042
Antony Denyer <antonydenyer@hotmail.com>
2012-04-24 21:25:44 +0100

    write the step definition in js p1

---
+++ features/step_definitions/homepage.js
@@ -0,0 +1,18 @@
+var hompageSteps = function() {
+    this.Given(/^I am on the lnug homepage$/, function(callback) {
+        // express the regexp above with the code you wish you had
+        callback();
+    });
+
+    this.Given(/^I register for the next lnug event$/, function(callback) {
+        // express the regexp above with the code you wish you had
+        callback();
+    });
+
+    this.Then(/^I should be taken to eventbrite$/, function(callback) {
+        // express the regexp above with the code you wish you had
+        callback();
+    });
+
+}
+module.exports = hompageSteps;
